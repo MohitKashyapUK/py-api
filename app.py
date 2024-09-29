6fa51b20603d3e3e7fa0f5c5abe941ca45076f24
Mohit Kashyap <mohitkashyap46707@gmail.com>
2024-09-29 13:14:12 +0530

    gg

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from html import escape
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return """<b>/frame</b>: Returns a frame from a video URL by given timestamp."""
+
+@app.route("/frame")
+def frame():
+    return f"<h1>/ frame / {escape('<ISO timestamp (DD:HH:MM:SS)>')} / {escape('<video URL>')}</h1>"
