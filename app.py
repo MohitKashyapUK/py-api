c0d7b850e24df3080e9b007cce71ab64ef0c2fef
Mohit Kashyap <mohitkashyap46707@gmail.com>
2024-09-29 12:50:46 +0530

    dev-3

--- app.py
+++ app.py
@@ -56,11 +56,11 @@ def isoFormatToMS(duration):
 
     return image_data'''
 
-@app.get("/")
+@app.route("/")
 def index():
     return """<b>/frame</b>: Returns a frame from a video URL by given timestamp."""
 
-@app.get("/frame")
+@app.route("/frame")
 def frame():
     return f"<h1>/ frame / {escape('<ISO timestamp (DD:HH:MM:SS)>')} / {escape('<video URL>')}</h1>"
 
